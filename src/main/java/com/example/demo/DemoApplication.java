629c5b1915fec3a0593e3753c0136cd2d9f1c787
Jiang Zhihao <jiangzhihao.jzh@antgroup.com>
2021-08-03 11:32:30 +0800

    Add some bugs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Hello World");
+        int x = 10;
+        x = x = 5;
+        System.out.println("Hello World: " + x);
     }
 
 }
